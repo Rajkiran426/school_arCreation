d82dda3ff827359cf1a4efdacd431948042da483
rajkiran426 <razzsingh426@gmail.com>
2024-05-18 12:30:30 +0530

    Added new code

--- src/app/staffs/staffs.component.ts
+++ src/app/staffs/staffs.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./staffs.component.scss']
 })
 export class StaffsComponent {
+  images1 = [
+    { url: '../../assets/images/pic1.jpeg', title: 'Name', description: 'About' },
+    { url: 'image2.jpg', title: 'Image 2', description: 'Description for Image 2' },
+    { url: 'image3.jpg', title: 'Image 3', description: 'Description for Image 3' },
+  ];
+  images2 = [
+    { url: 'image1.jpg', title: 'Image 1', description: 'Description for Image 1' },
+    { url: 'image2.jpg', title: 'Image 2', description: 'Description for Image 2' },
+    { url: 'image3.jpg', title: 'Image 3', description: 'Description for Image 3' },
+  ];
 
 }
